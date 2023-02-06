1fc3d75d8de935b5d22522a5fd2bb9119b60d8ea
Nawed Alam <nawedalam890@gmail.com>
2023-02-06 21:04:34 +0530

    Day 1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
-void main()
-{
+
+void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child :Center(
-        child :Container(
-        child: Text("Welcome to first app!"),
-      ),
-      ),
+      //     home: Material(
+      //       child :Center(
+      //       child :Container(
+      //       child: Text("Welcome to first app!"),
+      //     ),
+      //     ),
+      //     ),
+      //   );
+
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Catlog App"),
+        ), 
+        body: Center(
+          child: Container(
+            child: Text("Welcome to first app!"),
+          ),
+        ),
+        drawer: Drawer(),
       ),
     );
   }
